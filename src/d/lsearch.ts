f7b21f84a4045fc774dff3a3614db70194da9ee9
dyu <david.yu.ftw@gmail.com>
2017-07-12 18:39:17 +0800

    lsearch

---
+++ src/d/lsearch.ts
@@ -0,0 +1,18 @@
+import { Opts, parseOpts, cleanup } from '../_lsearch'
+
+export function update(this: any, value: any, oldValue: any) {
+    if (oldValue) return
+
+    if (value && value.pager && value.fields) {
+        this.lsearch = parseOpts(this.arg && this.arg.split(','),
+                value.pager, value.fields, value.fn, this.vm, this.el)
+    } else {
+        console.warn('v-lsearch requires the value: { pager: Pager, fields: string[], fn? }')
+    }
+}
+
+export function unbind(this: any) {
+    let lsearch: Opts = this.lsearch
+    if (lsearch)
+        cleanup(lsearch)
+}
---
+++ src/d2/lsearch.ts
@@ -0,0 +1,22 @@
+import { VNodeDirective, VNodeWithData } from '../v2/'
+import { defp } from 'coreds/lib/util'
+import { Opts, parseOpts, cleanup } from '../_lsearch'
+
+export function inserted(el: any, dir: VNodeDirective, vnode: VNodeWithData) {
+    let value = dir.value
+
+    if (value && value.pager && value.fields) {
+        defp(el, 'lsearch_', parseOpts(dir.arg && dir.arg.split(','),
+                value.pager, value.fields, value.fn, vnode.context, el))
+    } else {
+        console.warn('v-lsearch requires the value: { pager: Pager, fields: string[], fn? }')
+    }
+}
+
+export function unbind(el: any, dir: VNodeDirective, vnode: VNodeWithData) {
+    let opts: Opts = el.lsearch_
+    if (opts) {
+        cleanup(opts)
+        el.lsearch_ = null
+    }
+}
