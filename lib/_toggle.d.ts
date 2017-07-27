1584cb13aaa9fa38ef9d3f9a79a13240d1c95e7d
dyu <david.yu.ftw@gmail.com>
2017-07-28 00:35:05 +0800

    generated

--- lib/_toggle.d.ts
+++ lib/_toggle.d.ts
@@ -6,8 +6,12 @@ export interface Opts {
     vm: any;
     el: any;
     handler: any;
+    keyup: any;
     index: number;
     prevIndex: number | null;
+}
+export declare const enum ToggleFlags {
+    ESC_ON_PARENT = 1,
 }
 export declare function parseOpts(args: string[] | any, target: any, vm: any, el: any): Opts;
 export declare function cleanup(opts: Opts): void;
