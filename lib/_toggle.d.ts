36b9a9ee87902b99caa837f3c79e7c096c92b7f8
dyu <david.yu.ftw@gmail.com>
2017-08-18 19:56:19 +0800

    generated

--- lib/_toggle.d.ts
+++ lib/_toggle.d.ts
@@ -1,7 +1,7 @@
 export interface Opts {
     type: string;
     flags: number;
-    focus_id: string;
+    focus_ref: string;
     focus_el: any;
     target: any;
     array: any;
@@ -15,6 +15,7 @@ export interface Opts {
 }
 export declare const enum ToggleFlags {
     ESC_ON_PARENT = 1,
+    FOCUS_REF_AS_ID = 2,
 }
 export declare function parseOpts(args: string[] | any, target: any, vm: any, el: any): Opts;
 export declare function cleanup(opts: Opts): void;
